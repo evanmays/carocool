e8fa9aada993d9aecfcb892181e9c7f61e7c96f0
Evan Mays <evan@rootbeer.computer>
2023-01-29 17:53:09 -0800

    save a bunch of stuff

---
+++ navigation.js
@@ -0,0 +1,10 @@
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const navItems = document.querySelector(".nav-items");
+const img = document.querySelector("img");
+const container = document.querySelector(".container");
+
+hamburgerMenu.addEventListener("click", () => {
+  hamburgerMenu.classList.toggle("change");
+  navItems.classList.toggle("open");
+  container.classList.toggle("close")
+});
