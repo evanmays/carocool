3e6bfece81e69358b7738c25f48e4341ee8d56bb
Evan Mays <evan@rootbeer.computer>
2023-09-17 15:27:40 -0700

    new design

--- navigation.js
+++ navigation.js
@@ -1,9 +1,23 @@
+const nametag = document.querySelector("#nameitalics");
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const navItems = document.querySelector(".nav-items");
 const img = document.querySelector("img");
 const container = document.querySelector(".container");
+const footer = document.querySelector(".footer");
 
 hamburgerMenu.addEventListener("click", () => {
+  if (hamburgerMenu.classList.contains("change")) {
+    nametag.classList.remove("change");
+    hamburgerMenu.classList.remove("change");
+    navItems.classList.remove("open");
+    img.classList.remove("close");
+    container.classList.remove("close");
+    footer.classList.remove("close");
+  }
+});
+
+nametag.addEventListener("click", () => {
+  nametag.classList.toggle("change");
   hamburgerMenu.classList.toggle("change");
   navItems.classList.toggle("open");
   if (img) {
@@ -11,5 +25,8 @@ hamburgerMenu.addEventListener("click", () => {
   }
   if (container) {
     container.classList.toggle("close");
+  }
+  if (footer) {
+    footer.classList.toggle("close");
   }
 });
